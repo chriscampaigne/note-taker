e242fd6eb83c696f8b9f5467e7eb1cbfff19eea5
chriscampaigne <campaignemc@gmail.com>
2022-08-28 10:58:58 -0400

    added static method and listen method in server.js

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ const PORT = process.env.PORT || 3001;
 //instantiates the server
 const app = express();
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data, 
+app.use(express.json());
+
+// makes files "static resources", meaning all front end code can be accessed w/o a specific endpoint
+app.use(express.static('public'));
 const apiRoutes = require('./Routes/apiRoutes');
 const htmlRoutes = require('./Routes/htmlRoutes');
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
