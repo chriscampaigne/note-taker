62c9ced5fc1b951ff53d56a2edc4a0752bb750db
chriscampaigne <campaignemc@gmail.com>
2022-08-28 10:29:11 -0400

    add heroku

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
 //instantiates the server
 const app = express();
 
+const apiRoutes = require('./Routes/apiRoutes');
+const htmlRoutes = require('./Routes/htmlRoutes');
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+  });
