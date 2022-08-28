8b5118c8eaf6875209b22851598502f959acb2c5
chriscampaigne <campaignemc@gmail.com>
2022-08-28 11:54:11 -0400

    added post and get for apiroute

---
+++ Routes/apiRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+// returns notes as json
+const saveData = require('../db/saveData');
+
+// GET request
+router.get('/notes', function (req, res) {
+    saveData
+        .retrieveNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
+});
+
+// POST request
+router.post('/notes', (req, res) => {
+    saveData
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch(err => res.status(500).json(err));
+});
+
+module.exports = router;
